2af26227c9f5174fcf2405358474291f475bd4d3
graceyudhaaa <graceyudhaaa@students.unnes.ac.id>
2019-11-14 18:08:44 +0700

    update lagi

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-const CACHE_NAME = "newsreader-v4";
+const CACHE_NAME = "firstpwa-v1";
 var urlsToCache = [
   "/",
   "/nav.html",
   "/index.html",
-  "/article.html", 
+  "/article.html",
   "/pages/home.html",
   "/pages/about.html",
   "/pages/contact.html",
   "/css/materialize.min.css",
   "/js/materialize.min.js",
+  "/manifest.json",
+  "/js/nav.js",
   "/js/api.js",
-  "/js/nav.js"
+  "/icon.png"
 ];
- 
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
@@ -23,6 +25,7 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("fetch", function(event) {
   var base_url = "https://readerapi.codepolitan.com/";
+
   if (event.request.url.indexOf(base_url) > -1) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
@@ -34,24 +37,24 @@ self.addEventListener("fetch", function(event) {
     );
   } else {
     event.respondWith(
-        caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-            return response || fetch (event.request);
-        })
-    )
-}
-});
-  
-  self.addEventListener("activate", function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheName != CACHE_NAME) {
-              console.log("ServiceWorker: cache " + cacheName + " dihapus");
-              return caches.delete(cacheName);
-            }
-          })
-        );
+      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+        return response || fetch (event.request);
       })
-    );
-  });
+    )
+  }
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
